3c3296f33215526067d31db85b4a8313d6756c8d
5-mahri <71532978+5-mahri@users.noreply.github.com>
2020-10-13 01:12:30 +0200

    Add files via upload

---
+++ Question_5.java
@@ -0,0 +1,21 @@
+package assignment6;
+
+public class Question_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   			
+ 	   for (int row=1; row<=7; row++) {
+			int lastNumber=row;
+			for(int p=7-row; p>=1; p--) {
+   				System.out.print("1");}
+		       for(int i=1; i<=lastNumber; i++) {
+		    	   
+			    System.out.print(lastNumber);
+		       }
+		       System.out.println();
+		}}
+		 
+	}
+
+
