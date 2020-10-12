64400e3dbea144f69c0377fe58a041420bdf8dbc
5-mahri <71532978+5-mahri@users.noreply.github.com>
2020-10-13 01:08:33 +0200

    Add files via upload

---
+++ Question_1.java
@@ -0,0 +1,19 @@
+package assignment6;
+
+public class Question_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1; i<=7; i++) {
+			for(int p=1; p<=i; p++) {
+					System.out.print(p+" ");
+				
+				}	System.out.println();
+			}
+	  for(int j=6; j>=1; j--) {
+		  for(int m=1; m<=j; m++) {
+			  System.out.print(m+" ");
+		  }
+		  System.out.println();
+	  }
+}}
